dd2e051c6d246725f9b69bf01680f27b8d4c9f9f
cinderblockgames <79210192+cinderblockgames@users.noreply.github.com>
2021-04-22 13:51:32 -0400

    Remove inline js (#6)

--- web/assets/js/site.js
+++ web/assets/js/site.js
@@ -1,4 +1,59 @@
 (function($) {
+  // ---------------------------------
+  // menu scrolling
+  // ---------------------------------
+  //according to loftblog tut
+  $(".main-menu li:first").addClass("active");
+
+  var showSection = function showSection(section, isAnimate) {
+    var direction = section.replace(/#/, ""),
+      reqSection = $(".section").filter(
+        '[data-section="' + direction + '"]'
+      ),
+      reqSectionPos = reqSection.offset().top - 0;
+
+    if (isAnimate) {
+      $("body, html").animate(
+        {
+          scrollTop: reqSectionPos
+        },
+        800
+      );
+    } else {
+      $("body, html").scrollTop(reqSectionPos);
+    }
+  };
+
+  var checkSection = function checkSection() {
+    $(".section").each(function() {
+      var $this = $(this),
+        topEdge = $this.offset().top - 80,
+        bottomEdge = topEdge + $this.height(),
+        wScroll = $(window).scrollTop();
+      if (topEdge < wScroll && bottomEdge > wScroll) {
+        var currentId = $this.data("section"),
+          reqLink = $("a").filter("[href*=\\#" + currentId + "]");
+        reqLink
+          .closest("li")
+          .addClass("active")
+          .siblings()
+          .removeClass("active");
+      }
+    });
+  };
+
+  $(".main-menu").on("click", "a", function(e) {
+    e.preventDefault();
+    showSection($(this).attr("href"), true);
+  });
+
+  $(window).scroll(function() {
+    checkSection();
+  });
+  // ---------------------------------
+  // end menu scrolling
+  // ---------------------------------
+
   var toggle = document.getElementById("menu-toggle");
   var menu = document.getElementById("menu");
   var close = document.getElementById("menu-close");
